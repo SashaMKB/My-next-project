7ab949f928b040141e1f72f789918c16bd356750
Alexander <73365442+SashaMKB@users.noreply.github.com>
2022-03-22 12:10:14 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTask
+{
+    class Class1
+    {
+        public event EventHandler<char> OnKeyPressed;
+        public void Run()
+        { 
+        while(true)
+            {
+                var EnteredButton= Console.ReadKey();
+                if (EnteredButton.Key == ConsoleKey.C)
+                {
+                    Console.WriteLine("\nExit.");
+                    break;
+                }
+                else OnKeyPressed?.Invoke(this, EnteredButton.KeyChar);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NewTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Class1 = new Class1();
+            Class1.OnKeyPressed += (sender, letter) => Console.WriteLine($"\nYou entered {letter}");
+            Class1.Run();
+        }
+    }
+}
